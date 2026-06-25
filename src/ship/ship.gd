6ccdd19b7df798364e3d83c29f98445024c9e5d8
Nathan Fightmaster <nmfightmaster@gmail.com>
2026-06-25 14:59:56 -0700

    feat: update Ship for movement and combat

--- src/ship/ship.gd
+++ src/ship/ship.gd
@@ -1,15 +1,26 @@
 class_name Ship
-extends Resource
+extends RefCounted
 
-@export_category("Stats")
-@export var health: int
-@export var speed: float
-@export var acceleration: float
-@export var capacitor_capacity: int
+var health: int
+var position: int
+var facing: int = 1:
+	set(value):
+		assert(value == 1 or value == -1)
+		facing = value
 
-@export_category("Cards")
-@export var max_hand_size: int
-@export var cards_drawn: int
+func move(distance: int) -> void:
+	position += facing * distance
 
-@export_category("Movement")
-@export var position: Vector3
+func turn() -> void:
+	facing *= -1
+
+func take_damage(amount: int) -> void:
+	health = max(0, health - amount)
+
+func can_hit(target: Ship) -> bool:
+	return sign(target.position - position) == facing
+
+func _init(h: int, p: int = 0, f: int = 1) -> void:
+	health = h
+	position = p
+	facing = f
