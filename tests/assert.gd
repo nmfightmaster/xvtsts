80fd4ad437a7090aec331ebe43ce3e098b522d89
Nathan Fightmaster <nmfightmaster@gmail.com>
2026-06-25 14:59:23 -0700

    chore: colorize assert output

--- tests/assert.gd
+++ tests/assert.gd
@@ -6,20 +6,20 @@ var failures: int = 0
 
 func is_true(condition: bool, description: String) -> void:
 	if (condition):
-		print("%s -- PASS" % description)
+		print_rich("%s -- [color=green]PASS[/color]" % description)
 		passes += 1
 	else:
-		print("%s -- FAIL" % description)
+		print_rich("%s -- [color=red]FAIL[/color]" % description)
 		failures += 1
 	
 
 func is_eq(actual: Variant, expected: Variant, description: String) -> void:
 	if (actual == expected):
-		print("%s -- PASS" % description)
+		print_rich("%s -- [color=green]PASS[/color]" % description)
 		passes += 1
 	else:
-		print("%s -- FAIL -- expected: %s, got: %s" % [description, expected, actual])
+		print_rich("%s -- [color=red]FAIL[/color] -- expected: %s, got: %s" % [description, expected, actual])
 		failures += 1
 
 func summarize() -> void:
-	print("Total passed: %s \nTotal failed: %s" % [passes, failures])
+	print_rich("Total passed: [color=green]%s[/color] \nTotal failed: [color=red]%s[/color]" % [passes, failures])
