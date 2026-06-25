173659b70ad57b900e02245ffb1553136ea729b7
Nathan Fightmaster <nmfightmaster@gmail.com>
2026-06-24 19:03:55 -0700

    chore: add ship acceleration, speed, and position

--- src/ship/ship.gd
+++ src/ship/ship.gd
@@ -1,7 +1,15 @@
 class_name Ship
 extends Resource
 
+@export_category("Stats")
 @export var health: int
+@export var speed: float
+@export var acceleration: float
 @export var capacitor_capacity: int
+
+@export_category("Cards")
 @export var max_hand_size: int
 @export var cards_drawn: int
+
+@export_category("Movement")
+@export var position: Vector3
