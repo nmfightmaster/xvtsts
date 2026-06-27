a82c9400f32fbd990843edc183398a22ad2ee40d
Nathan Fightmaster <nmfightmaster@gmail.com>
2026-06-27 13:08:43 -0700

    feat: add tests for movement_turn

---
+++ tests/movement/test_movement_turn.gd
@@ -0,0 +1,41 @@
+class_name TestMovementTurn
+
+static func run() -> void:
+	test_get_actions()
+	test_get_ships()
+	test_clear()
+	Assert.summarize_single("MovementTurn")
+
+static func test_get_actions() -> void:
+	var s1: Ship = Ship.new(1)
+	var ma1: MovementAction = MovementAction.new(MovementAction.Type.MOVE, 1)
+	var s2: Ship = Ship.new(1)
+	var ma2: MovementAction = MovementAction.new(MovementAction.Type.MOVE, 1)
+	var sa: Array[Ship] = [s1, s2]
+	var maa1: Array[MovementAction] = [ma1]
+	var maa2: Array[MovementAction] = [ma2]
+	var mt: MovementTurn = MovementTurn.new(sa)
+	mt.append(s1, maa1)
+	mt.append(s2, maa2)
+	Assert.is_true(mt.get_actions(s1) == maa1 and mt.get_actions(s2) == maa2, "Can assign [MovementAction] for multiple Ships without clobbering")
+
+static func test_get_ships() -> void:
+	var s1: Ship = Ship.new(1)
+	var s2: Ship = Ship.new(1)
+	var sa: Array[Ship] = [s1, s2]
+	var mt: MovementTurn = MovementTurn.new(sa)
+	Assert.is_eq(mt.get_ships(), sa, "Can return all registered Ships") # assumes insertion order!
+
+static func test_clear() -> void:
+	var s1: Ship = Ship.new(1)
+	var ma1: MovementAction = MovementAction.new(MovementAction.Type.MOVE, 1)
+	var s2: Ship = Ship.new(1)
+	var ma2: MovementAction = MovementAction.new(MovementAction.Type.MOVE, 1)
+	var sa: Array[Ship] = [s1, s2]
+	var maa1: Array[MovementAction] = [ma1]
+	var maa2: Array[MovementAction] = [ma2]
+	var mt: MovementTurn = MovementTurn.new(sa)
+	mt.append(s1, maa1)
+	mt.append(s2, maa2)
+	mt.clear(s1)
+	Assert.is_true(mt.get_actions(s1) == [] and mt.get_actions(s2) == maa2, "Can clear [MovementAction] for Ship without clobbering")
