fdcbc2ce3540842d5d57accf01c433d88bc9b9f9
Nathan Fightmaster <nmfightmaster@gmail.com>
2026-06-27 11:21:21 -0700

    refactor: enum facing

--- src/ship/ship.gd
+++ src/ship/ship.gd
@@ -3,24 +3,26 @@ extends RefCounted
 
 var health: int
 var position: int
-var facing: int = 1:
+
+enum Facing {POS = 1, NEG = -1}
+var facing: Facing = Facing.POS:
 	set(value):
-		assert(value == 1 or value == -1)
+		assert(value == Facing.POS or value == Facing.NEG)
 		facing = value
 
 func move(distance: int) -> void:
 	position += facing * distance
 
 func turn() -> void:
-	facing *= -1
+	facing = Facing.NEG if facing == Facing.POS else Facing.POS
 
 func take_damage(amount: int) -> void:
 	health = max(0, health - amount)
 
 func can_hit(target: Ship) -> bool:
-	return sign(target.position - position) == facing
+	return (target.position - position) * facing > 0
 
-func _init(h: int, p: int = 0, f: int = 1) -> void:
+func _init(h: int, p: int = 0, f: Facing = Facing.POS) -> void:
 	health = h
 	position = p
 	facing = f
