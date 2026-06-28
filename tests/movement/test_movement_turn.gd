16737222ce159fcd71463258af3964cb87f7fdd0
Nathan Fightmaster <nmfightmaster@gmail.com>
2026-06-28 12:41:00 -0700

    fix: harden tests for movement turn

--- tests/movement/test_movement_turn.gd
+++ tests/movement/test_movement_turn.gd
@@ -4,6 +4,7 @@ static func run() -> void:
 	test_get_actions()
 	test_get_ships()
 	test_clear()
+	test_accumulation()
 	Assert.summarize_single("MovementTurn")
 
 static func test_get_actions() -> void:
@@ -39,3 +40,14 @@ static func test_clear() -> void:
 	mt.append(s2, maa2)
 	mt.clear(s1)
 	Assert.is_true(mt.get_actions(s1) == [] and mt.get_actions(s2) == maa2, "Can clear [MovementAction] for Ship without clobbering")
+
+static func test_accumulation() -> void:
+	var s: Ship = Ship.new(1)
+	var ma1: MovementAction = MovementAction.new(MovementAction.Type.MOVE, 1)
+	var ma2: MovementAction = MovementAction.new(MovementAction.Type.MOVE, 2)
+	var maa1: Array[MovementAction] = [ma1]
+	var maa2: Array[MovementAction] = [ma2]
+	var mt: MovementTurn = MovementTurn.new([s])
+	mt.append(s, maa1)
+	mt.append(s, maa2)
+	Assert.is_eq(mt.get_actions(s), [ma1, ma2], "Can append multiple arrays of MovementActions")
