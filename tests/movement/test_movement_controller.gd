828bc58645ca5ce28a5c8e6b3ec155094b76db66
Nathan Fightmaster <nmfightmaster@gmail.com>
2026-06-28 12:34:28 -0700

    fix: harden tests for movement controller

--- tests/movement/test_movement_controller.gd
+++ tests/movement/test_movement_controller.gd
@@ -5,6 +5,8 @@ static func run() -> void:
 	test_frame_indexing()
 	test_single_ship_move()
 	test_single_ship_turn()
+	test_cumulative_moves()
+	test_turn_then_move()
 	test_mismatched_action_count()
 	Assert.summarize_single("TestMovementController")
 
@@ -36,6 +38,24 @@ static func test_single_ship_turn() -> void:
 	mt.append(s, [ma])
 	var frames = MovementController.get_frames(mt)
 	Assert.is_eq(frames[0].combined_frame[s].facing, Ship.Facing.NEG, "Single TURN action produces frame with correct facing")
+
+static func test_cumulative_moves() -> void:
+	var s: Ship = Ship.new(1)
+	var ma1: MovementAction = MovementAction.new(MovementAction.Type.MOVE, 1)
+	var ma2: MovementAction = MovementAction.new(MovementAction.Type.MOVE, 2)
+	var mt: MovementTurn = MovementTurn.new([s])
+	mt.append(s, [ma1, ma2])
+	var frames = MovementController.get_frames(mt)
+	Assert.is_eq(frames[1].combined_frame[s].position, 3, "Multiple moves properly affect position properly")
+
+static func test_turn_then_move() -> void:
+	var s: Ship = Ship.new(1)
+	var ma1: MovementAction = MovementAction.new(MovementAction.Type.TURN)
+	var ma2: MovementAction = MovementAction.new(MovementAction.Type.MOVE, 2)
+	var mt: MovementTurn = MovementTurn.new([s])
+	mt.append(s, [ma1, ma2])
+	var frames = MovementController.get_frames(mt)
+	Assert.is_eq(frames[1].combined_frame[s].position, -2, "Turning them moving affects position properly")
 
 static func test_mismatched_action_count() -> void:
 	var s1: Ship = Ship.new(1)
